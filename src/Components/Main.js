5350e3165b35055cf7f26e148ab834029dcaaf31
Mike Eddards <mike.eddards@codecorp.com>
2020-01-16 16:55:56 -0700

    started main page

---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <header className='header'>
+                    <img className='logo' src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/https://www.codecorp.com/wp-content/uploads/2019/09/CODE-LOGO-EXPECT-MORE-144x70.png" alt="code logo" />
+                    <h1>SKU Finder</h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+
+
